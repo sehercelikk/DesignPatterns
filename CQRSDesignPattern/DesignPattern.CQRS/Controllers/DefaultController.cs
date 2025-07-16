7b0ddd964128c5173bc93074615c22cd139a4501
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-07-16 17:07:45 +0300

    Add files via upload

---
+++ CQRSDesignPattern/DesignPattern.CQRS/Controllers/DefaultController.cs
@@ -0,0 +1,67 @@
+﻿using DesignPattern.CQRS.CQRSPattern.Command;
+using DesignPattern.CQRS.CQRSPattern.Handler;
+using DesignPattern.CQRS.CQRSPattern.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.CQRS.Controllers;
+
+public class DefaultController : Controller
+{
+    private readonly GetProductQueryHandler _getProductQueryHandler;
+    private readonly CreateProductCommandHandler _createProductCommandHandler;
+    private readonly GetProductByIdQueryHandler _getProductByIdQueryHandler;
+    private readonly DeleteProductCommandHandler _deleteProductCommandHandler;
+    private readonly UpdateProductCommandHandler _updateProductCommandHandler;
+    private readonly PostUpdateProductCommandHandler _postUpdateProductCommandHandler;
+    public DefaultController(GetProductQueryHandler getProductQueryHandler, CreateProductCommandHandler createProductCommandHandler, GetProductByIdQueryHandler getProductByIdQueryHandler, DeleteProductCommandHandler deleteProductCommandHandler, UpdateProductCommandHandler updateProductCommandHandler, PostUpdateProductCommandHandler postUpdateProductCommandHandler)
+    {
+        _getProductQueryHandler = getProductQueryHandler;
+        _createProductCommandHandler = createProductCommandHandler;
+        _getProductByIdQueryHandler = getProductByIdQueryHandler;
+        _deleteProductCommandHandler = deleteProductCommandHandler;
+        _updateProductCommandHandler = updateProductCommandHandler;
+        _postUpdateProductCommandHandler = postUpdateProductCommandHandler;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _getProductQueryHandler.Handle();
+        return View(values);
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public IActionResult Create(CreateProductCommand command)
+    {
+        _createProductCommandHandler.Handle(command);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult GetProduct(int id)
+    {
+        var value = _getProductByIdQueryHandler.Handle(new GetProductByIdQuery(id));
+        return View(value);
+    }
+
+    public IActionResult DeleteProduct(int id)
+    {
+        _deleteProductCommandHandler.Handle(new DeleteProductCommand(id));
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Update(int id)
+    {
+        var values= _updateProductCommandHandler.Handle(new GetProductUpdateIdQuery(id));
+        return View(values);
+    }
+
+    [HttpPost]
+    public IActionResult Update(UpdateProductCommand command)
+    {
+        _postUpdateProductCommandHandler.Handle(command);
+        return RedirectToAction("Index");
+    }
+
+
+}
---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.CQRS.DAL;
+
+public class Context : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=BPODSHGM74\MSSQLSERVER2025; initial Catalog=CQRS; integrated security=true;TrustServerCertificate=True;");
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
