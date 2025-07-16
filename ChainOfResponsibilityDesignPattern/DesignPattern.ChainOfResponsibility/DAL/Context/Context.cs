7630f3e0d94724b19a240abc868b2d24d96089a7
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-07-16 17:15:09 +0300

    Update Context.cs

--- ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context/Context.cs
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context/Context.cs
@@ -7,7 +7,7 @@ public class Context : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=BPODSHGM74\MSSQLSERVER2025; initial Catalog=ChainRespons; integrated security=true;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(@"Server=YoureServerName; initial Catalog=ChainRespons; integrated security=true;TrustServerCertificate=True;");
     }
 
     public DbSet<CustomerProcess> CustomerProcesses { get; set; }
