08451d15e068e846e4e03e05a29eac09aeb04eba
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-07-16 17:14:29 +0300

    Update Context.cs

--- CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -6,7 +6,7 @@ public class Context : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=BPODSHGM74\MSSQLSERVER2025; initial Catalog=CQRS; integrated security=true;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(@"Server=YoureServerName; initial Catalog=CQRS; integrated security=true;TrustServerCertificate=True;");
     }
 
     public DbSet<Product> Products { get; set; }
