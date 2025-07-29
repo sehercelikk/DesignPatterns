73a289e442a25743efc1afd10308cdb92c2073d4
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-07-29 13:35:55 +0300

    Add files via upload

---
+++ DesignPattern.Facade/FacadePattern/OrderFacade.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern;
+
+public class OrderFacade
+{
+    Order order=new Order();
+    OrderDetail orderDetail=new OrderDetail();
+    ProductStock productStock=new ProductStock();
+
+    AddOrder addOrder=new AddOrder();
+    AddOrderDetail addOrderDetail=new AddOrderDetail();
+    public void ComplateOrderDetail(int customerId, int productId,int orderId, int productCount, decimal productPrice)
+    {
+        orderDetail.CustomerId = customerId;
+        orderDetail.ProductId = productId;
+        orderDetail.ProductCount = productCount;
+        orderDetail.ProductPrice = productPrice;
+        decimal totalProductPrice = productCount * productCount;
+        orderDetail.ProductTotalPrice = totalProductPrice;
+
+        orderDetail.OrderId = orderId;
+        addOrderDetail.AddNewOrderDetail(orderDetail);
+
+        productStock.StockDecrease(productId, productCount);
+
+    }
+    public void ComplateOrder(int customerId)
+    {
+        order.CustomerId = customerId;
+        addOrder.AddNewOrder(order);
+    }
+}
---
+++ DesignPattern.Facade/Migrations/20250729065533_mig3.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPattern.Facade.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: true),
+                    ProductCount = table.Column<int>(type: "int", nullable: false),
+                    ProductPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductTotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId");
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId");
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_CustomerId",
+                table: "OrderDetails",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+        }
+    }
+}
