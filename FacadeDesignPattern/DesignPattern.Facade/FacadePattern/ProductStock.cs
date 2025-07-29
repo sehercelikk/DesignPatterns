e72d1ff42d126244950d4d3bb86b6c15937093a3
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-07-29 13:37:06 +0300

    Add files via upload

---
+++ FacadeDesignPattern/DesignPattern.Facade/FacadePattern/ProductStock.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern;
+
+public class ProductStock
+{
+    Context  context= new Context();
+    public void StockDecrease(int id, int amount)
+    {
+        var value = context.Products.Find(id);
+        value.Stock-=amount;
+        context.SaveChanges();
+    }
+}
---
+++ FacadeDesignPattern/DesignPattern.Facade/Migrations/20250729063020_mig2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPattern.Facade.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
